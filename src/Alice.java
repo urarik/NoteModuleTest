c5cbbaa611c4efb27df4ad30216c3d318850574f
urarik <a01076684995@gmail.com>
2022-04-10 22:38:43 +0900

    Test for member init

--- src/Alice.java
+++ src/Alice.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Alice implements India{
-    private List<String> names;
+    private List<String> names = new ArrayList<>();
     private Go go;
 
     @Override
