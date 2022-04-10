0d57424afc5999b8b8e35f204042d222d9c501f8
urarik <a01076684995@gmail.com>
2022-04-10 21:22:27 +0900

    Test for import

---
+++ src/testing/ImportTest.java
@@ -0,0 +1,7 @@
+package testing;
+
+import something.Bob;
+
+public class ImportTest {
+    private Bob bob;
+}
---
+++ src/testing/ImportTest2.java
@@ -0,0 +1,7 @@
+package testing;
+
+import something.*;
+
+public class ImportTest2 {
+    protected Bob bob;
+}
