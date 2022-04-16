97919a40fc6ca0a6601026fbda4703cdaa75777f
urarik <a01076684995@gmail.com>
2022-04-16 19:15:15 +0900

    Test for conditional statement

--- src/Bob.java
+++ src/Bob.java
@@ -3,6 +3,14 @@ public class Bob {
     protected Go getGo(String str) {
         Echo echo = new Echo(new Fruit());
 
+        if(echo == null) {
+            System.out.println("!!@#");
+        } else if(echo instanceof Echo) {
+            System.out.println("!@#");
+        } else {
+            int i = 3 + 2;
+        }
+
         return new Go();
     }
 }
